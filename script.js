cf450d483298fcfd2c70230c36651688a9e58c20
Molly Obermeier <obermeier.mol@gmail.com>
2022-03-08 08:31:31 -0700

    8. Set interval fixed

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let score = 0
-let time = 6000
+let time = 5000
 const trash = document.querySelector('.trash')
 const tally = document.querySelector('.score')
 let randomTop
 let randomLeft
 
+trash.addEventListener('click', () => {
+  score += 2
+  tally.innerText = score
+  randomLocation()
+})
 //developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 randomLocation = () => {
   min = Math.ceil(80)
@@ -16,36 +21,45 @@ randomLocation = () => {
 }
 
 // https://www.w3schools.com/js/js_operators.asp
-timeSet = (score) => {
+timeSet = () => {
   if (score <= 10) {
-    time = 5000; 
-  } if else (score > 10 && score <= 20) {
-    time = 4000
-  } if else (score > 20 && score <= 30) {
     time = 3000
-  } if else (score > 30 && score <= 40) {
-    time = 2000
-  } if else (score > 40 && score <= 50) {
+  } else if (score > 10 && score <= 20) {
     time = 1000
-  } if else (score > 50 && score <= 60) {
+    clearInterval(trashTime)
+    trashTime = setInterval(playGame, time)
+  } else if (score > 20 && score <= 30) {
+    time = 200
+    clearInterval(trashTime)
+    trashTime = setInterval(playGame, time)
+  } else if (score > 30 && score <= 40) {
+    time = 2000
+    clearInterval(trashTime)
+    trashTime = setInterval(playGame, time)
+  } else if (score > 40 && score <= 50) {
+    time = 1000
+    clearInterval(trashTime)
+    trashTime = setInterval(playGame, time)
+  } else if (score > 50 && score <= 60) {
     time = 500
+    clearInterval(trashTime)
+    trashTime = setInterval(playGame, time)
   }
+}
+
+let playGame = () => {
+  randomLocation()
+  timeSet()
 }
 // https://javascript.info/settimeout-setinterval
 // let trashTimer = setInterval(randomLocation, timeSet)
 
-let trashTime = setTimeout(randomLocation, timeSet)
+let trashTime = setInterval(playGame, time)
 
 // if (score >= 10) {
 //   timeSet -= 4000
 //   console.log(timeSet)
 // }
-
-trash.addEventListener('click', () => {
-  score += 2
-  tally.innerText = score
-  randomLocation()
-})
 
 // function trashMove = () => {
 //   trashX = math.floor(math.random() * gridArr)
