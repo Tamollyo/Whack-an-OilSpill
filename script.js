2af0ce0bd14b4201440713d5b9028d135ea13b1f
Molly Obermeier <obermeier.mol@gmail.com>
2022-03-08 08:03:20 -0700

    7. Updates to CSS + timing

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let score = 0
+let time = 6000
 const trash = document.querySelector('.trash')
 const tally = document.querySelector('.score')
 let randomTop
@@ -14,8 +15,31 @@ randomLocation = () => {
   trash.style.left = randomLeft + '%'
 }
 
+// https://www.w3schools.com/js/js_operators.asp
+timeSet = (score) => {
+  if (score <= 10) {
+    time = 5000; 
+  } if else (score > 10 && score <= 20) {
+    time = 4000
+  } if else (score > 20 && score <= 30) {
+    time = 3000
+  } if else (score > 30 && score <= 40) {
+    time = 2000
+  } if else (score > 40 && score <= 50) {
+    time = 1000
+  } if else (score > 50 && score <= 60) {
+    time = 500
+  }
+}
 // https://javascript.info/settimeout-setinterval
-let trashTimer = setInterval(randomLocation, 1000)
+// let trashTimer = setInterval(randomLocation, timeSet)
+
+let trashTime = setTimeout(randomLocation, timeSet)
+
+// if (score >= 10) {
+//   timeSet -= 4000
+//   console.log(timeSet)
+// }
 
 trash.addEventListener('click', () => {
   score += 2
